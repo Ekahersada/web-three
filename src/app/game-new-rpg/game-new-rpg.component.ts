11a65a016c3469708abdf4b182044a7f83fbd81e
Eka hersada <ekahersada@gmail.com>
2024-11-07 16:00:39 +0700

    updates

--- src/app/game-new-rpg/game-new-rpg.component.ts
+++ src/app/game-new-rpg/game-new-rpg.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls, GLTFLoader, PointerLockControls  } from 'three/examples/jsm/Addons.js';
 
 @Component({
   selector: 'app-game-new-rpg',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-new-rpg.component.css']
 })
 export class GameNewRpgComponent implements OnInit {
+  @ViewChild('rendererContainer2', { static: true }) rendererContainer!: ElementRef;
+
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
 
   constructor() { }
 
