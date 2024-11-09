461888a73c57e54cdc7b3f53637da24b4f2ec2c3
Eka hersada <ekahersada@gmail.com>
2024-11-09 15:02:06 +0700

    updates

--- src/app/game-new-rpg/game-new-rpg.component.ts
+++ src/app/game-new-rpg/game-new-rpg.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { OrbitControls, GLTFLoader, PointerLockControls  } from 'three/examples/jsm/Addons.js';
+import { PreloaderInit, PreloaderOptions } from './preloader';
 
 @Component({
   selector: 'app-game-new-rpg',
@@ -9,14 +10,200 @@ import { OrbitControls, GLTFLoader, PointerLockControls  } from 'three/examples/
 })
 export class GameNewRpgComponent implements OnInit {
   @ViewChild('rendererContainer2', { static: true }) rendererContainer!: ElementRef;
+  @ViewChild('container', { static: true }) container!: ElementRef;
 
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
 
+  anims = ["run", "gather-objects", "look-around"];
+
+  modes = {
+    NONE: "none",
+    PRELOAD: "preload",
+    INITIALIZE: "initialize",
+  }
+
+  mode = this.modes.NONE;
+
+
+  clock = new THREE.Clock();
+
   constructor() { }
 
   ngOnInit() {
+
+
+
+    this.mode = this.modes.PRELOAD;
+
+    const ass =  this.anims.map(anim => `assets/lotus/fbx/${anim}.fbx`)
+
+    console.log(ass);
+  // Initialize preloader
+  const preloaderOptions: PreloaderOptions = {
+    assets: this.anims.map(anim => `assets/lotus/fbx/${anim}.fbx`),
+    container: this.container.nativeElement,
+    onprogress: (progress: number) => {
+      console.log(progress);
+    },
+    oncomplete: () => {
+
+      console.log("Preloading complete");
+      this.mode = this.modes.INITIALIZE;
+      this.initScene();
+      this.runAnimate();
+    }
+  };
+
+  const preloader = new PreloaderInit(preloaderOptions);
+
+  }
+
+  // MARK: Animate
+  runAnimate(){
+    requestAnimationFrame(() => this.runAnimate());
+
+    this.renderer.render(this.scene, this.camera);
+
+
+  }
+
+  // MARK: Initialize the scene
+  initScene(){
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xa8def0);
+
+
+    // Create a grid
+    var gridHelper = new THREE.GridHelper(40, 40);
+    this.scene.add(gridHelper);
+
+
+    // Initialize camera
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.set(0, 1, 0);// Set camera position 0.1 units above the grid
+
+    // Adjust the camera's near clipping plane value
+    this.camera.near = .015; // Set a smaller value, like 0.1
+    this.camera.updateProjectionMatrix();
+
+
+    this.generateFloor();
+
+    this.addLight()
+  }
+  // MARK: Add light
+  addLight(){
+    // LIGHTS
+    
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 2 );
+    hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 50, 0 );
+        this.scene.add( hemiLight );
+    
+    const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+        this.scene.add( hemiLightHelper );
+    
+    //
+    
+    const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
+    dirLight.color.setHSL( 0.1, 1, 0.95 );
+    dirLight.position.set( - 1, 1.75, 1 );
+    dirLight.position.multiplyScalar( 30 );
+        this.scene.add( dirLight );
+    
+    dirLight.castShadow = true;
+    
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+    
+    const d = 50;
+    
+    dirLight.shadow.camera.left = - d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = - d;
+    
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = - 0.0001;
+    
+    const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
+        this.scene.add( dirLightHelper );
+    
+    // GROUND
+    
+    const groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
+    const groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+    groundMat.color.setHSL( 0.095, 1, 0.75 );
+    
+    const ground = new THREE.Mesh( groundGeo, groundMat );
+    ground.position.y = - 33;
+    ground.rotation.x = - Math.PI / 2;
+    ground.receiveShadow = true;
+        this.scene.add( ground );
+  }
+  // MARK:Generate the floor
+  generateFloor(){
+
+    //Add LIGHT
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+    dirLight.position.set(- 60, 100, - 10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = - 50;
+    dirLight.shadow.camera.left = - 50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.mapSize.width = 4096;
+    dirLight.shadow.mapSize.height = 4096;
+    this.scene.add(dirLight);
+
+
+// TEXTURES
+const textureLoader = new THREE.TextureLoader();
+const placeholder = textureLoader.load("./assets/textures/placeholder/placeholder.png");
+const sandBaseColor = textureLoader.load("./assets/textures/sand/Sand 002_COLOR.jpg");
+const sandNormalMap = textureLoader.load("./assets/textures/sand/Sand 002_NRM.jpg");
+const sandHeightMap = textureLoader.load("./assets/textures/sand/Sand 002_DISP.jpg");
+const sandAmbientOcclusion = textureLoader.load("./assets/textures/sand/Sand 002_OCC.jpg");
+
+const WIDTH = 80
+const LENGTH = 80
+
+const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
+const material = new THREE.MeshStandardMaterial(
+   {
+       map: sandBaseColor, normalMap: sandNormalMap,
+       displacementMap: sandHeightMap, displacementScale: 0.1,
+       aoMap: sandAmbientOcclusion
+   })
+this.wrapAndRepeatTexture(material.map!)
+this.wrapAndRepeatTexture(material.normalMap!)
+this.wrapAndRepeatTexture(material.displacementMap!)
+this.wrapAndRepeatTexture(material.aoMap!)
+// const material = new THREE.MeshPhongMaterial({ map: placeholder})
+
+const floor = new THREE.Mesh(geometry, material)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI / 2
+this.scene.add(floor)
+  }
+
+  wrapAndRepeatTexture(map: THREE.Texture){
+  map.wrapS = map.wrapT = THREE.RepeatWrapping
+  map.repeat.x = map.repeat.y = 10;
+
+  return map;
   }
 
 }
---
+++ src/app/game-new-rpg/preloader.ts
@@ -0,0 +1,152 @@
+
+export interface PreloaderOptions {
+    assets: string[];
+    container?: HTMLElement;
+    onprogress?: (progress: number) => void;
+    oncomplete?: () => void;
+}
+
+interface Asset {
+    loaded: number;
+    complete: boolean;
+    total?: number;
+}
+
+export class PreloaderInit {
+    assets: { [key: string]: Asset };
+    container?: HTMLElement;
+    domElement?: HTMLElement;
+    progressBar?: HTMLElement;
+    onprogress: (progress: number) => void;
+    oncomplete: () => void;
+
+    constructor(options: PreloaderOptions) {
+        this.assets = {};
+        for (let asset of options.assets) {
+            this.assets[asset] = { loaded: 0, complete: false };
+            this.load(asset);
+        }
+        this.container = options.container;
+
+
+
+        if (options.onprogress === undefined) {
+            this.onprogress = this.defaultOnProgress;
+            this.createProgressBar();
+
+            console.log('undefined');
+        } else {
+            this.onprogress = options.onprogress;
+            this.createProgressBar();
+
+
+        }
+
+        this.oncomplete = options.oncomplete || (() => {});
+    }
+
+    private createProgressBar() {
+        this.domElement = document.createElement("div");
+        this.domElement.style.position = 'absolute';
+        this.domElement.style.top = '0';
+        this.domElement.style.left = '0';
+        this.domElement.style.width = '100%';
+        this.domElement.style.height = '100%';
+        this.domElement.style.background = '#000';
+        this.domElement.style.opacity = '0.7';
+        this.domElement.style.display = 'flex';
+        this.domElement.style.alignItems = 'center';
+        this.domElement.style.justifyContent = 'center';
+        this.domElement.style.zIndex = '1111';
+
+        const barBase = document.createElement("div");
+        barBase.style.background = '#aaa';
+        barBase.style.width = '50%';
+        barBase.style.minWidth = '250px';
+        barBase.style.borderRadius = '10px';
+        barBase.style.height = '15px';
+        this.domElement.appendChild(barBase);
+
+        const bar = document.createElement("div");
+        bar.style.background = '#2a2';
+        bar.style.borderRadius = '10px';
+        bar.style.height = '100%';
+        bar.style.width = '0';
+        barBase.appendChild(bar);
+
+        this.progressBar = bar;
+
+        if (this.container == undefined) {
+            document.body.appendChild(this.domElement);
+        } else {
+            this.container.appendChild(this.domElement);
+        }
+
+        
+
+        console.log(this.container)
+      
+    }
+
+    private defaultOnProgress(progress: number) {
+        if (this.progressBar) {
+            const progressPercentage = progress * 100;
+            this.progressBar.style.width = `${progressPercentage}%`;
+        }
+    }
+
+    private checkCompleted(): boolean {
+        for (let prop in this.assets) {
+            const asset = this.assets[prop];
+            if (!asset.complete) return false;
+        }
+        return true;
+    }
+
+    get progress(): number {
+        let total = 0;
+        let loaded = 0;
+
+        for (let prop in this.assets) {
+            const asset = this.assets[prop];
+            if (asset.total === undefined) {
+                loaded = 0;
+                break;
+            }
+            loaded += asset.loaded;
+            total += asset.total;
+        }
+
+        return loaded / total;
+    }
+
+    private load(url: string) {
+        
+        const loader = this;
+        const xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', url, true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState === 4 && xobj.status === 200) {
+                loader.assets[url].complete = true;
+                if (loader.checkCompleted()) {
+                    if (loader.domElement !== undefined) {
+                        if (loader.container !== undefined && loader.container.contains(loader.domElement)) {
+                            loader.container.removeChild(loader.domElement);
+                        } else if (document.body.contains(loader.domElement)) {
+                            document.body.removeChild(loader.domElement);
+                        }
+                    }
+                    loader.oncomplete();
+                }
+            }
+        };
+        xobj.onprogress = function (e) {
+            const asset = loader.assets[url];
+            asset.loaded = e.loaded;
+            asset.total = e.total;
+            loader.onprogress(loader.progress);
+        };
+        xobj.send(null);
+    }
+}
